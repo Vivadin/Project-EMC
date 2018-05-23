776d93f075839cce6dd08b1e117426db58dde03b
Vivadin <me@vivadin.com>
2018-05-23 14:30:47 +0100

    Refactored blocks into their own library

--- src/main/java/com/vivadin/projectemc/init/RegistryHandlers.java
+++ src/main/java/com/vivadin/projectemc/init/RegistryHandlers.java
@@ -1,6 +1,7 @@
 package com.vivadin.projectemc.init;
 
 import com.vivadin.projectemc.common.registry.ObjectRegistry;
+import com.vivadin.projectemc.objects.PemcBlocks;
 import com.vivadin.projectemc.objects.blocks.BlockEmcOre;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -12,16 +13,13 @@ import net.minecraftforge.registries.IForgeRegistry;
 @Mod.EventBusSubscriber
 public class RegistryHandlers extends ObjectRegistry
 {
-   // BLOCKS
-   public static BlockEmcOre emcOre;
-   
    @SubscribeEvent
    public static void registerBlocks(final RegistryEvent.Register<Block> event)
    {
       // Register the blocks
       final IForgeRegistry<Block> registry = event.getRegistry();
       
-      emcOre = registerBlock(registry, new BlockEmcOre(), "ore_overworld");
+      PemcBlocks.emcOre = registerBlock(registry, new BlockEmcOre(), "ore_overworld");
    }
    
    @SubscribeEvent
---
+++ src/main/java/com/vivadin/projectemc/objects/PemcBlocks.java
@@ -0,0 +1,8 @@
+package com.vivadin.projectemc.objects;
+
+import com.vivadin.projectemc.objects.blocks.BlockEmcOre;
+
+public class PemcBlocks
+{
+   public static BlockEmcOre emcOre;
+}
