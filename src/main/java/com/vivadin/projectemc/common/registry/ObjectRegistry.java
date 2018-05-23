70a107630e16343e6fdd0839196261ed0e122753
Vivadin <me@vivadin.com>
2018-05-23 18:48:19 +0100

    Optimized the Block's Item registration process.. This is now automatic...!

--- src/main/java/com/vivadin/projectemc/common/registry/ObjectRegistry.java
+++ src/main/java/com/vivadin/projectemc/common/registry/ObjectRegistry.java
@@ -1,16 +1,25 @@
 package com.vivadin.projectemc.common.registry;
 
 import com.vivadin.projectemc.ProjectEmc;
+import com.vivadin.projectemc.common.ItemBlockVariant;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class ObjectRegistry
 {
+   // Somewhere to store multiple item blocks - as this is much easier than
+   // registering them one by one :S
+   private static List<Item> _itemBlocks = new ArrayList<Item>();
+   
+   
    protected static <T extends Block> T registerBlock(IForgeRegistry<Block> registry, T block, String name)
    {
       if (!name.equals(name.toLowerCase(Locale.US)))
@@ -20,7 +29,20 @@ public class ObjectRegistry
       
       block.setUnlocalizedName(name);
       register(registry, block, name);
+      _itemBlocks.add(
+         new ItemBlockVariant(block)
+            .setRegistryName(block.getRegistryName())
+      );
       return block;
+   }
+   
+   protected static void registerItemsFromBlocks(IForgeRegistry<Item> registry)
+   {
+      registry.registerAll(
+         _itemBlocks.toArray(
+            new Item[0]
+         )
+      );
    }
    
    protected static <T extends Item> T registerItem(IForgeRegistry<Item> registry, T item, String name)
--- src/main/java/com/vivadin/projectemc/init/RegistryHandlers.java
+++ src/main/java/com/vivadin/projectemc/init/RegistryHandlers.java
@@ -6,11 +6,15 @@ import com.vivadin.projectemc.objects.PemcBlocks;
 import com.vivadin.projectemc.objects.blocks.BlockEmcOre;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber
 public class RegistryHandlers extends ObjectRegistry
@@ -25,17 +29,20 @@ public class RegistryHandlers extends ObjectRegistry
    }
    
    @SubscribeEvent
-   public static void registerItemBlocks(final RegistryEvent.Register<Item> event)
+   public static void registerItems(final RegistryEvent.Register<Item> event)
    {
       // Register the Items7
       final IForgeRegistry<Item> registry = event.getRegistry();
       
-      registerItem(
-         registry,
-         new ItemBlockVariant(PemcBlocks.emcOre),
+      // New block item registration function
+      registerItemsFromBlocks(registry);
+      
+      //registerItem(
+      //   registry,
+      //   new ItemBlockVariant(PemcBlocks.emcOre),
            // .setRegistryName(PemcBlocks.emcOre.getRegistryName()),
-         "ore_overworld"
-      );
+      //   "ore_overworld"
+      //);
    }
    
    @SubscribeEvent
